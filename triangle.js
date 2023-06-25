e8b3670f699dbb1a590a7e25a4b139de05588b74
Courage OmniBarry <135624085+AndreOmniBarry@users.noreply.github.com>
2023-06-25 09:58:01 +0100

    Add files via upload

---
+++ triangle.js
@@ -0,0 +1,11 @@
+//A=1/2(base * height) 
+const base = 10;
+  const height = 20;
+
+function calculateTriangleArea (base, height) {
+   area = (base * height) ;
+   return area;
+  }
+  let result = (base * height /2 );
+
+    console.log("The area is " + result);
